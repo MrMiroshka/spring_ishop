12ce50360c99ffb0285d551db88338ff01786c5c
Miroshnichenko Igor <Miroshnichenko.Igor.Valerivich@yandex.ru>
2023-01-29 16:39:35 +0500

    hw7 Выполнено тестирование бина CartService Выполднено тестирование api CartController Переписал тесты с использованнием мокито

--- hw7/cart-service/src/test/java/ru/miroshka/market/carts/controllers/CartControllerTest.java
+++ hw7/cart-service/src/test/java/ru/miroshka/market/carts/controllers/CartControllerTest.java
@@ -2,9 +2,12 @@ package ru.miroshka.market.carts.controllers;
 
 import org.junit.jupiter.api.Assertions;
 import  org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoSettings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import ru.miroshka.market.api.dto.ProductDto;
@@ -14,6 +17,7 @@ import ru.miroshka.market.carts.integrations.ProductServiceIntegration;
 import ru.miroshka.market.carts.models.Cart;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -32,19 +36,24 @@ class CartControllerTest {
     @Autowired
     Cart cart;
 
-    @Autowired
+    @MockBean
     ProductServiceIntegration productServiceIntegration;
+
+
 
     @Test
     void getCurrentCart() {
 
         //т.к. микросервис, то запускаем отдельно core-service - там есть несколько продуктов
+        // (закомментировано - это тест без мокито) - но нужно закооментировать  @MockBean
         //1) берем из продукт и ложим в корзину
         //2) запрашиваем  корзину
         //3) сравниваем нашу корзину после добавления туда продукта и запрошенную корзину
 
+        //4) Не закомментированно работает через макито
 
-        CartDto responseDtoInput = webTestClient.get()
+
+/*        CartDto responseDtoInput = webTestClient.get()
                 .uri("/api/v1/cart/add/1")
                 .exchange()
                 //.expectStatus().isOk()
@@ -83,7 +92,30 @@ class CartControllerTest {
             Assertions.assertEquals(responseDtoInput.getItems().get(i).getClass(),
                     responseDto.getItems().get(i).getClass());
 
-        }
+        }*/
+
+
+        Mockito.when(productServiceIntegration.getProductById(13L)).thenReturn(
+                Optional.of(new ProductDto(13L, "test", new BigDecimal("1"))));
+
+        CartDto responseDtoInput = webTestClient.get()
+                .uri("/api/v1/cart/add/13")
+                .exchange()
+                //.expectStatus().isOk()
+                .expectBody(CartDto.class)
+                .returnResult()
+                .getResponseBody()
+                ;
+
+        CartDto responseDto = webTestClient.get()
+                .uri("/api/v1/cart")
+                .exchange()
+                //.expectStatus().isOk()
+                .expectBody(CartDto.class)
+                .returnResult()
+                .getResponseBody()
+        ;
+        Assertions.assertEquals("test",responseDto.getItems().get(0).getProductTitle());
 
 
         //   Assertions.assertEquals(cartConverter.entityToDto(cart).getTotalPrice(),responseDto.getTotalPrice());
@@ -92,12 +124,14 @@ class CartControllerTest {
     @Test
     void putProductToCast() {
         //т.к. микросервис, то запускаем отдельно core-service - там есть несколько продуктов
+        // (закомментировано - это тест без мокито) - но нужно закооментировать  @MockBean
         //1) берем из продукт и ложим в корзину
         //2) запрашиваем  корзину
         //3) сравниваем нашу корзину после добавления туда продукта и запрошенную корзину
 
+        //4) Не закомментированно работает через макито
 
-        CartDto responseDtoInput = webTestClient.get()
+  /*      CartDto responseDtoInput = webTestClient.get()
                 .uri("/api/v1/cart/add/1")
                 .exchange()
                 //.expectStatus().isOk()
@@ -137,6 +171,28 @@ class CartControllerTest {
                     responseDto.getItems().get(i).getClass());
 
         }
+*/
+        Mockito.when(productServiceIntegration.getProductById(13L)).thenReturn(
+                Optional.of(new ProductDto(13L, "test", new BigDecimal("1"))));
+
+        CartDto responseDtoInput = webTestClient.get()
+                .uri("/api/v1/cart/add/13")
+                .exchange()
+                //.expectStatus().isOk()
+                .expectBody(CartDto.class)
+                .returnResult()
+                .getResponseBody()
+                ;
+
+        CartDto responseDto = webTestClient.get()
+                .uri("/api/v1/cart")
+                .exchange()
+                //.expectStatus().isOk()
+                .expectBody(CartDto.class)
+                .returnResult()
+                .getResponseBody()
+                ;
+        Assertions.assertEquals("test",responseDto.getItems().get(0).getProductTitle());
 
 
     }
@@ -144,7 +200,7 @@ class CartControllerTest {
     @Test
     void deleteProductBasket() {
 
-        webTestClient.get()
+/*        webTestClient.get()
                 .uri("/api/v1/cart/add/1")
                 .exchange()
                 //.expectStatus().isOk()
@@ -204,14 +260,64 @@ class CartControllerTest {
             Assertions.assertEquals(responseDtoInput.getItems().get(i).getPricePerProduct(),
                     responseDto.getItems().get(i).getPricePerProduct());
             Assertions.assertEquals(responseDtoInput.getItems().get(i).getClass(),
-                    responseDto.getItems().get(i).getClass());
+                    responseDto.getItems().get(i).getClass());*/
 
 
+        Mockito.when(productServiceIntegration.getProductById(1L)).thenReturn(
+                Optional.of(new ProductDto(1L, "test1", new BigDecimal("1"))));
 
+        Mockito.when(productServiceIntegration.getProductById(2L)).thenReturn(
+                Optional.of(new ProductDto(2L, "test2", new BigDecimal("2"))));
+
+        webTestClient.get()
+                .uri("/api/v1/cart/add/1")
+                .exchange()
+                //.expectStatus().isOk()
+                .expectBody(CartDto.class)
+                .returnResult()
+                .getResponseBody()
+        ;
+
+        CartDto responseDtoInput = webTestClient.get()
+                .uri("/api/v1/cart/add/2")
+                .exchange()
+                //.expectStatus().isOk()
+                .expectBody(CartDto.class)
+                .returnResult()
+                .getResponseBody()
+                ;
+
+        Assertions.assertEquals(3,responseDtoInput.getItems().size());
+
+        webTestClient.get()
+                .uri("/api/v1/cart/delete/13")
+                .exchange()
+                //.expectStatus().isOk()
+                .expectBody(CartDto.class)
+                .returnResult()
+                .getResponseBody()
+        ;
+
+        CartDto responseDto = webTestClient.get()
+                .uri("/api/v1/cart")
+                .exchange()
+                //.expectStatus().isOk()
+                .expectBody(CartDto.class)
+                .returnResult()
+                .getResponseBody()
+                ;
+        Assertions.assertEquals("test1",responseDto.getItems().get(0).getProductTitle());
+        Assertions.assertEquals(2,responseDto.getItems().size());
     }
 
     @Test
     void deleteAllProductBasket() {
+        Mockito.when(productServiceIntegration.getProductById(1L)).thenReturn(
+                Optional.of(new ProductDto(1L, "test1", new BigDecimal("1"))));
+
+        Mockito.when(productServiceIntegration.getProductById(2L)).thenReturn(
+                Optional.of(new ProductDto(2L, "test2", new BigDecimal("2"))));
+
         webTestClient.get()
                 .uri("/api/v1/cart/add/1")
                 .exchange()
